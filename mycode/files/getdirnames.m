3dbbfa5dfdf4f11396f34cf310a437ef6a5ffc83
Jean-Francois Lalonde <jf.lalonde@disneyresearch.com>
2012-12-17 15:21:20 -0500

    Added option of recursively looking for directories within sub-directories.
    Not extensively tested. Should return only the bottom-level sub-directories (not their parents).

--- mycode/files/getdirnames.m
+++ mycode/files/getdirnames.m
@@ -1,4 +1,4 @@
-function files = getdirnames(path, pattern, fullpath)
+function dirs = getdirnames(path, pattern, fullpath, recurse)
 % Return cell array of directories matching the pattern at the specified path.
 % 
 %   files = getdirnames(basePath, pattern, fullpath)
@@ -7,6 +7,9 @@ function files = getdirnames(path, pattern, fullpath)
 % 
 %   'fullpath': boolean (defaults to false). When 'true', returns the full
 %   path for each file.
+%
+%   'recurse': integer which specifies the number of times we have to
+%   recurse into sub-directories.
 %   
 % 
 % ----------
@@ -20,15 +23,32 @@ if nargin < 3
     fullpath = false;
 end
 
-files = dir(fullfile(path, pattern));
-dirInd = [files(:).isdir];
-files = {files(dirInd).name};
+if nargin < 4 
+    recurse = 0;
+end
+
+dirs = dir(fullfile(path, pattern));
+dirInd = [dirs(:).isdir];
+dirs = {dirs(dirInd).name};
 
 % remove hidden directories
-hiddenFilesInd = cellfun(@(x) x(1)=='.', files);
-files(hiddenFilesInd) = [];
+hiddenFilesInd = cellfun(@(x) x(1)=='.', dirs);
+dirs(hiddenFilesInd) = [];
 
 % concatenate full path to each file
 if fullpath
-    files = fullfile(path, files);
+    dirs = fullfile(path, dirs);
+end
+
+if recurse > 0
+    allDirs = {};
+    for i_d = 1:length(dirs)
+        curDir = dirs{i_d};
+        if ~fullpath
+            curDir = fullfile(path, curDir);
+        end
+        allDirs = cat(2, allDirs, ...
+            getdirnames(curDir, pattern, fullpath, recurse-1));
+    end
+    dirs = allDirs;
 end
