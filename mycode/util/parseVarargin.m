ac9457508b9d19bbc74daf338685791b61fed737
Jean-Francois Lalonde <jf.lalonde@disneyresearch.com>
2013-03-18 16:49:44 -0400

    When there are no input arguments, display list of acceptable arguments.
    This is obtained from looking at all the variables currently available (in the caller's workspace).

--- mycode/util/parseVarargin.m
+++ mycode/util/parseVarargin.m
@@ -18,6 +18,37 @@ function parseVarargin(varargin)
 % ----------
 % Jean-Francois Lalonde
 
+if mod(nargin,2)==1
+    % first argument will specify whether to display list or not when there
+    % are no other arguments
+    displayArgs = varargin{1};
+    varargin = varargin(2:end);
+else
+    % defaults to displaying the arguments
+    displayArgs = true;
+end
+    
+
+narginCaller = evalin('caller', 'nargin');
+if narginCaller == 0 && displayArgs
+    % Display list of possible arguments and their types
+    args = evalin('caller', 'whos');
+    
+    % TODO: parse the file and automatically retrieve the comments directly
+    % above the line where the argument is declared. Output it below. 
+    fprintf('Possible optional arguments: \n');
+    for i_arg=1:length(args)
+        % ignore 'varargin'
+        if ~strcmp(args(i_arg).name, 'varargin')
+            fprintf('  %s (%s)\n', args(i_arg).name, args(i_arg).class);
+        end
+    end
+    
+    error('parseVarargin:displayArgs', ...
+        ['Call function again with valid parameters.' ...
+        '\n (this error is normal -- blame Matlab''s inability to stop execution cleanly.)']);
+end
+
 for i = 1:2:length(varargin)
     if ~(evalin('caller', sprintf('exist(''%s'')', varargin{i}))==1)
         error('Parameter "%s" not recognized.', varargin{i});
