254e21715a3892e571b4a1f63a43212a902fda6c
Jean-Francois Lalonde <jf.lalonde@disneyresearch.com>
2013-03-18 09:31:58 -0400

    Pulled out input/output parsing functions.

--- mycode/color/rgb2srgb.m
+++ mycode/color/rgb2srgb.m
@@ -12,41 +12,7 @@ function varargout = rgb2srgb(varargin)
 % ----------
 % Jean-Francois Lalonde
 
-inputType = 0; 
-if nargin == 1
-    if ndims(varargin{1}) == 3
-        % we're given an image
-        R = varargin{1}(:,:,1);
-        G = varargin{1}(:,:,2);
-        B = varargin{1}(:,:,3);
-        
-        inputType = 1;
-    elseif ismatrix(varargin{1})
-        % we're given a 3xN array
-        assert(size(varargin{1}, 1) == 3, ...
-            'Input must be of size 3xN');
-        
-        R = varargin{1}(1,:);
-        G = varargin{1}(2,:);
-        B = varargin{1}(3,:);
-        
-        inputType = 2;
-        
-    else
-        error('Input must have either 2 or 3 dimensions');
-    end
-    
-elseif nargin == 3
-    % we're given the 3 channels independently
-    R = varargin{1};
-    G = varargin{2};
-    B = varargin{3};
-    
-    inputType = 3;
-    
-else
-    error('Need either one or 3 inputs');
-end
+[R, G, B, type] = parseColorInput(varargin{:});
 
 % apply the colorspace transformation
 R = srgbTrans(R);
@@ -61,24 +27,6 @@ B = srgbTrans(B);
         C(C>t)  = (1+a).*C.^(1/2.4)-a;
     end
 
-switch inputType
-    case 1
-        % image
-        [nrows, ncols, nbands] = size(varargin{1});
-        R = reshape(R, [nrows, ncols, nbands]);
-        G = reshape(G, [nrows, ncols, nbands]);
-        B = reshape(B, [nrows, ncols, nbands]);
-        varargout{1} = cat(3, R, G, B);
-        
-    case 2
-        % 3xN vector
-        varargout{1} = cat(1, row(R), row(G), row(B));
-        
-    case 3
-        % independent channels
-        varargout{1} = reshape(R, size(varargin{1}));
-        varargout{2} = reshape(G, size(varargin{2}));
-        varargout{3} = reshape(B, size(varargin{3}));
-end
+varargout = parseColorOutput(R, G, B, type, varargin{:});
 
 end
