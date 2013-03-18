748a0cb1bef163db7f5589b9d31e355577501b39
Jean-Francois Lalonde <jf.lalonde@disneyresearch.com>
2013-03-18 09:34:31 -0400

    Updated RGB2XYZ conversion function.

--- mycode/color/rgb2xyz.m
+++ mycode/color/rgb2xyz.m
@@ -1,35 +1,20 @@
-function [X,Y,Z] = rgb2xyz(R,G,B)
+function varargout = rgb2xyz(varargin)
 % Converts RGB to XYZ data.
 %
-%   xyz = rgb2xyz(rgb)
-% 
-% Converts between images.
+%   img = rgb2xyz(img)
+%   vec = rgb2xyz(vec)
+%   [x,y,z] = rgb2xyz(r, g, b)
 %
-%   [x, y, z] = rgb2xyz(r, g, b)
-%
-% Converts between individual channels.
+% Takes in image, vector (3xN) or independent channels.
 %
 % See http://en.wikipedia.org/wiki/CIE_1931_Color_Space
 %
 % ----------
 % Jean-Francois Lalonde
 
-if (nargin == 1)
-    B = R(:,:,3);
-    G = R(:,:,2);
-    R = R(:,:,1);
-end
-
-[m, n] = size(R);
+[R,G,B,type] = parseColorInput(varargin{:});
 
 M = [0.412453 0.357580 0.180423; 0.212671 0.715160 0.072169; 0.019334 0.119193 0.950227];
-
 res = M * [R(:)'; G(:)'; B(:)'];
 
-X = reshape(res(1,:), m, n);
-Y = reshape(res(2,:), m, n);
-Z = reshape(res(3,:), m, n);
-
-if ((nargout == 1) || (nargout == 0))
-    X = cat(3,X,Y,Z);
-end
+varargout = parseColorOutput(res(1,:), res(2,:), res(3,:), type, varargin{:});
