b6ef7fe8569723a5047d1c4f01d03d0d6e7caace
Jean-Francois Lalonde <jflalonde@gmail.com>
2013-11-29 14:50:54 -0500

    Added simple caching function.

---
+++ mycode/cache/cacheFunction.m
@@ -0,0 +1,33 @@
+function varargout = cacheFunction(fnHandle, varargin)
+% Main 'caching' function.
+%
+%   [...] = cacheFunction(fnHandle, ...)
+%
+% 
+% ----------
+% Jean-Francois Lalonde
+%
+
+h = hashKey(varargin{:});
+
+cachePath = getPathName('results', 'cache');
+cacheFile = fullfile(cachePath, func2str(fnHandle), [h '.mat']);
+
+if exist(cacheFile, 'file')
+    fprintf('Found in cache. Retrieving...\n'); tic;
+    
+    % has been cached already! just load from cache
+    results = loadSorted(cacheFile);
+    varargout = results.results(1:nargout);
+else
+    % has not yet been cached. compute (and store) it.
+    fprintf('Not found in cache. Computing...'); tic;
+    
+    results = cell(1, nargout);
+    [results{:}] = fnHandle(varargin{:}); %#ok
+    [~,~,~] = mkdir(fileparts(cacheFile));
+    save(cacheFile, 'results');
+    
+    fprintf('done in %2.fs\n', toc);
+end
+    
