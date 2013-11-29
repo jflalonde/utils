8cc4f562a74ef7a0e93370681e49fa0dfe1853be
Jean-Francois Lalonde <jflalonde@gmail.com>
2013-11-29 11:21:21 -0500

    Made hash key computation much (much!) faster for large cells/structs.
    Trick is to convert to a ginormous string before calling md5 (that was the slow part)

--- mycode/cache/hashKey.m
+++ mycode/cache/hashKey.m
@@ -13,11 +13,8 @@ function h = hashKey(input)
 
 if iscell(input)
     % we're dealing with a cell array
-    % hash each component independently, then combine the hashes
-    hs = cellfun(@hashKey, input, 'UniformOutput', false);
-    hs = cat(2, hs{:});
-    
-    h = hashKeyFromString(hs);
+    % this may be big, so we'll first convert to string, then hash
+    h = hashKey(hashKeyHelper(input));
     
 elseif isnumeric(input) || islogical(input)
     % just convert to string
@@ -26,6 +23,20 @@ elseif isnumeric(input) || islogical(input)
 elseif ischar(input)
     % we have a string already
     h = hashKeyFromString(input);
+    
+elseif isstruct(input)
+    if isempty(input)
+        % Empty struct
+        h = hashKey('empty.struct');
+        
+    elseif isfield(input, 'hashKey') && ~isempty(input.hashKey)
+        % Check if there's already a hash key in there
+        h = input.hashKey;
+        
+    else
+        % Convert to string first, then call the hash key on it.
+        h = hashKey(hashKeyHelper(input));
+    end
     
 else
     % at this point, we don't support anything else
@@ -36,7 +47,19 @@ end
 function h = hashKeyFromString(str)
 % Helper: call md5 to generate a hash key from a string
 
-[r,h] = system(sprintf('md5 -q -s ''%s''', str));
+% if the string is too large, write it to a file otherwise it doesn't seem
+% to work. What's the maximum string size though?
+if length(str) > 1000
+    tmpFilename = tempname;
+    fid = fopen(tmpFilename, 'w');
+    fprintf(fid, '%s', str);
+    fclose(fid);
+    
+    [r,h] = system(sprintf('md5 -q %s', tmpFilename));
+
+else
+    [r,h] = system(sprintf('md5 -q -s ''%s''', str));
+end
 
 % strip trailing eol
 h = h(1:end-1);
