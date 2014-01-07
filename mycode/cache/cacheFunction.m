46f16027e822ec1f4ed2402e5b050a0afad0323e
Jean-Francois Lalonde <jflalonde@gmail.com>
2014-01-06 23:20:59 -0500

    Slightly more verbose cache function.

--- mycode/cache/cacheFunction.m
+++ mycode/cache/cacheFunction.m
@@ -14,14 +14,16 @@ cachePath = getPathName('results', 'cache');
 cacheFile = fullfile(cachePath, func2str(fnHandle), [h '.mat']);
 
 if exist(cacheFile, 'file')
-    fprintf('Found in cache. Retrieving...\n'); tic;
+    fprintf('Results of %s found in cache. Retrieving...\n', ...
+        func2str(fnHandle)); tic;
     
     % has been cached already! just load from cache
     results = loadSorted(cacheFile);
     varargout = results.results(1:nargout);
 else
     % has not yet been cached. compute (and store) it.
-    fprintf('Not found in cache. Computing...'); tic;
+    fprintf('Not found in cache. Computing %s...', ...
+        func2str(fnHandle)); tic;
     
     results = cell(1, nargout);
     [results{:}] = fnHandle(varargin{:});
