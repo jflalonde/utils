75934b58a8656888c47881b8b38ca3ba73c59883
Jean-Francois Lalonde <jflalonde@gmail.com>
2014-01-16 13:57:27 -0500

    Improved hash code with the help of CalcMD5.
    Thanks to Jan Simon for providing this code!
    See: http://www.mathworks.com/matlabcentral/fileexchange/25921-calcmd5

--- mycode/cache/hashKey.m
+++ mycode/cache/hashKey.m
@@ -1,4 +1,4 @@
-function h = hashKey(input)
+function h = hashKey(varargin)
 % Generates a hash key from the input
 %
 %   h = hashKey(input)
@@ -11,18 +11,29 @@ function h = hashKey(input)
 % ----------
 % Jean-Francois Lalonde
 
+if nargin > 1
+    allKeys = cell(1, nargin);
+    for i_arg = 1:nargin
+        allKeys{i_arg} = hashKey(varargin{i_arg});
+    end
+    h = hashKey(strcat(allKeys{:}));
+    return;
+end
+
+input = varargin{1};
+
 if iscell(input)
     % we're dealing with a cell array
     % this may be big, so we'll first convert to string, then hash
-    h = hashKey(hashKeyHelper(input));
+    s = cellfun(@hashKey, input, 'UniformOutput', false);
+    h = hashKey(cat(2, s{:}));
     
-elseif isnumeric(input) || islogical(input)
-    % just convert to string
-    h = hashKeyFromString(num2str(row(input)));
+elseif isnumeric(input) || ischar(input)
+    % run hash function directly
+    h = CalcMD5(input);
     
-elseif ischar(input)
-    % we have a string already
-    h = hashKeyFromString(input);
+elseif islogical(input)
+    h = CalcMD5(double(input));
     
 elseif isstruct(input)
     if isempty(input)
@@ -34,36 +45,12 @@ elseif isstruct(input)
         h = input.hashKey;
         
     else
-        % Convert to string first, then call the hash key on it.
-        h = hashKey(hashKeyHelper(input));
+        % Recurse
+        input = orderfields(input); % make field-order invariant
+        h = hashKey({struct2cell(input), fieldnames(input)});
     end
     
 else
     % at this point, we don't support anything else
     error('Unsupported input type');
-end
-
-
-function h = hashKeyFromString(str)
-% Helper: call md5 to generate a hash key from a string
-
-% if the string is too large, write it to a file otherwise it doesn't seem
-% to work. What's the maximum string size though?
-if length(str) > 1000
-    tmpFilename = tempname;
-    fid = fopen(tmpFilename, 'w');
-    fprintf(fid, '%s', str);
-    fclose(fid);
-    
-    [r,h] = system(sprintf('md5 -q %s', tmpFilename));
-
-else
-    [r,h] = system(sprintf('md5 -q -s ''%s''', str));
-end
-
-% strip trailing eol
-h = h(1:end-1);
-
-if r ~= 0
-    error('hashKey:md5', 'Error running the md5 command');
 end
